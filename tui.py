ca8de1163349a94a73b05cb31a0f0b1442bafa2f
KayleighPerera <71653944+KayleighPerera@users.noreply.github.com>
2020-12-10 14:33:41 +0100

    Task 1

--- tui.py
+++ tui.py
@@ -1,150 +1,149 @@
 def welcome():
-    print("------------------------------Solar Record Management System------------------------------")
-    Task 1: Display a welcome message.
+    # Task 1: Display a welcome message.
 
-    The welcome message should consist of the title 'Solar Record Management System' surrounded by dashes.
-    The number of dashes before and after the title should be equal to the number of characters in the title i.e. 30.
+    # The welcome message should consist of the title 'Solar Record Management System' surrounded by dashes.
+    # The number of dashes before and after the title should be equal to the number of characters in the title i.e. 30.
 
-    :return: Does not return anything.
+    # :return: Does not return anything.
     """
-    # TODO: Your code here
-
+    TODO: Your code here
+    print("------------------------------Solar Record Management System------------------------------")
+    return None
 
 def menu():
     """
-    Task 2: Display a menu of options and read the user's response.
+    # Task 2: Display a menu of options and read the user's response.
 
-    A menu consisting of the following options should be displayed:
-    'Load Data', 'Process Data', 'Visualise Data', 'Save Data' and 'Exit'
+    # A menu consisting of the following options should be displayed:
+    # 'Load Data', 'Process Data', 'Visualise Data', 'Save Data' and 'Exit'
 
-    The user's response should be read in and returned as an integer corresponding to the selected option.
-    For example, 1 for 'Load Data', 2 for 'Process Data' and so on.
+    # The user's response should be read in and returned as an integer corresponding to the selected option.
+    # For example, 1 for 'Load Data', 2 for 'Process Data' and so on.
 
-    If the user enters a invalid option then a suitable error message should be displayed and the value
-    None should be returned.
+    # If the user enters a invalid option then a suitable error message should be displayed and the value
+    # None should be returned.
 
-    :return: None if invalid selection otherwise an integer corresponding to a valid selection
+    # :return: None if invalid selection otherwise an integer corresponding to a valid selection
     """
     # TODO: Your code here
-
 
 def started(operation):
     """
-    Task 3: Display a message to indicate that an operation has started.
+    # Task 3: Display a message to indicate that an operation has started.
 
-    The function should display a message in the following format:
-    '{operation} has started.'
-    Where {operation} is the value of the parameter passed to this function
+    # The function should display a message in the following format:
+    #'{operation} has started.'
+    # Where {operation} is the value of the parameter passed to this function
 
-    :param operation: A string indicating the operation being started
-    :return: Does not return anything
+    # :param operation: A string indicating the operation being started
+    # :return: Does not return anything
     """
     # TODO: Your code here
-
+    
 
 def completed(operation):
     """
-    Task 4: Display a message to indicate that an operation has completed.
+    #Task 4: Display a message to indicate that an operation has completed.
 
-    The function should display a message in the following format:
-    '{operation} has completed.'
-    Where {operation} is the value of the parameter passed to this function
+    #The function should display a message in the following format:
+    # '{operation} has completed.'
+    # Where {operation} is the value of the parameter passed to this function
 
-    :param operation: A string indicating the operation being completed
-    :return: Does not return anything
+    #:param operation: A string indicating the operation being completed
+    #:return: Does not return anything
     """
     # TODO: Your code here
 
 
 def error(error_msg):
     """
-    Task 5: Display an error message.
+    #Task 5: Display an error message.
 
-    The function should display a message in the following format:
-    'Error! {error_msg}.'
-    Where {error_msg} is the value of the parameter passed to this function
+    #The function should display a message in the following format:
+    #'Error! {error_msg}.'
+    #Where {error_msg} is the value of the parameter passed to this function
 
-    :param error_msg: A string containing an error message
-    :return: Does not return anything
+    #:param error_msg: A string containing an error message
+    #:return: Does not return anything
     """
     # TODO: Your code here
 
 
 def source_data_path():
     """
-    Task 6: Retrieve a file path to the source data file.
+    #Task 6: Retrieve a file path to the source data file.
 
-    The function should prompt the user to enter the file path for a data file (e.g. 'data/sol_data.csv').
-    If the file path entered by the user does not end in 'csv' then a suitable error message should be displayed
-    and the value None should be returned.
-    Otherwise, the file path entered by the user should be returned.
+    #The function should prompt the user to enter the file path for a data file (e.g. 'data/sol_data.csv').
+    #If the file path entered by the user does not end in 'csv' then a suitable error message should be displayed
+    #and the value None should be returned.
+    #Otherwise, the file path entered by the user should be returned.
 
-    :return: None if the file path does not end in 'csv' otherwise return the file path entered by the user
+    #:return: None if the file path does not end in 'csv' otherwise return the file path entered by the user
     """
     # TODO: Your code here
 
 
 def process_type():
     """
-    Task 7: Display a menu of options for how the file should be processed. Read in the user's response.
+    #Task 7: Display a menu of options for how the file should be processed. Read in the user's response.
 
-    A menu should be displayed that contains the following options:
-        'Retrieve entity', 'Retrieve entity details', 'Categorise entities by type',
-        'Categorise entities by gravity', 'Summarise entities by orbit'
+    #A menu should be displayed that contains the following options:
+     #   'Retrieve entity', 'Retrieve entity details', 'Categorise entities by type',
+      #  'Categorise entities by gravity', 'Summarise entities by orbit'
 
-    The user's response should be read in and returned as an integer corresponding to the selected option.
-    For example, 1 for 'Retrieve entity', 2 for 'Retrieve entity details' and so on.
+    #The user's response should be read in and returned as an integer corresponding to the selected option.
+    #For example, 1 for 'Retrieve entity', 2 for 'Retrieve entity details' and so on.
 
-    If the user enters a invalid option then a suitable error message should be displayed and the value
-    None should be returned.
+    #If the user enters a invalid option then a suitable error message should be displayed and the value
+    #None should be returned.
 
-    :return: None if an invalid selection made otherwise an integer corresponding to a valid option
+    #:return: None if an invalid selection made otherwise an integer corresponding to a valid option
     """
     # TODO: Your code here
 
 
 def entity_name():
     """
-    Task 8: Read in the name of an entity and return the name.
+    #Task 8: Read in the name of an entity and return the name.
 
-    The function should ask the user to enter the name of an entity e.g. 'Earth'
-    The function should then read in and return the user's response.
+    #The function should ask the user to enter the name of an entity e.g. 'Earth'
+    #The function should then read in and return the user's response.
 
-    :return: the name of an entity
+    #:return: the name of an entity
     """
     # TODO: Your code here
 
 
 def entity_details():
     """
-    Task 9: Read in the name of an entity and column indexes. Return a list containing the name and indexes.
+    #Task 9: Read in the name of an entity and column indexes. Return a list containing the name and indexes.
 
-    The function should ask the user to enter the name of an entity e.g. 'Earth'
-    The function should also ask the user to enter a list of integer column indexes e.g. 0,1,5,7
-    The function should return a list containing the name of the entity and the list of column
-    indexes e.g. ['Earth', [0,1,5,7]]
+    #The function should ask the user to enter the name of an entity e.g. 'Earth'
+    #The function should also ask the user to enter a list of integer column indexes e.g. 0,1,5,7
+    #The function should return a list containing the name of the entity and the list of column
+    #indexes e.g. ['Earth', [0,1,5,7]]
 
-    :return: A list containing the name of an entity and a list of column indexes
+    #:return: A list containing the name of an entity and a list of column indexes
     """
     # TODO: Your code here
 
 
 def list_entity(entity, cols=[]):
     """
-    Task 10: Display an entity. Only the data for the specified column indexes will be displayed.
-    If no column indexes have been specified, then all the data for the entity will be displayed.
+    #Task 10: Display an entity. Only the data for the specified column indexes will be displayed.
+    #If no column indexes have been specified, then all the data for the entity will be displayed.
 
-    The entity is a list of values corresponding to particular Solar System space entity
-    E.g. ['Earth', TRUE, 9.8].
-    The function should only display those values from the entity list that correspond to the column
-    indexes provided as part of cols.
-    E.g. if cols is [0, 2] then for the entity ['Earth', TRUE, 9.8] the following will be displayed
-    ['Earth', 9.8]
-    E.g. if cols is an empty list then all the values will be displayed i.e. ['Earth', TRUE, 9.8]
+    #The entity is a list of values corresponding to particular Solar System space entity
+    #E.g. ['Earth', TRUE, 9.8].
+    #The function should only display those values from the entity list that correspond to the column
+    #indexes provided as part of cols.
+    #E.g. if cols is [0, 2] then for the entity ['Earth', TRUE, 9.8] the following will be displayed
+    #['Earth', 9.8]
+    #E.g. if cols is an empty list then all the values will be displayed i.e. ['Earth', TRUE, 9.8]
 
-    :param entity: A list of data values related to an entity
-    :param cols: A list of integer values that represent column indexes
-    :return: does not return anything
+    #:param entity: A list of data values related to an entity
+    #:param cols: A list of integer values that represent column indexes
+    #:return: does not return anything
     """
     # TODO: Your code here
 
