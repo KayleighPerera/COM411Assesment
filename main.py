445818f89649d959b07bab1831ef7335153cbd9b
KayleighPerera <71653944+KayleighPerera@users.noreply.github.com>
2020-12-15 21:48:49 +0100

    Task 17-20

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-# Task 17: Import the modules csv, tui and visual
+"""
+#Task 17: Import the modules csv, tui and visual
+"""
 # TODO: Your code here;
 
-from tui import tui
-from csv import csv
-from visual import visual
+import tui
+import csv
+import visual
 
-
+"""
 # Task 18: Create an empty list named 'records'.
 # This will be used to store the date read from the source data file.
 # TODO: Your code here
-
+"""
 records = []
 
+"""
 def run():
 
     # Task 19: Call the function welcome of the module tui.
     # This will display our welcome message when the program is executed.
-    # TODO: Your code here
-
+    #TODO: Your code here
+"""
+def run():
     from tui import welcome
-
 run()
 
-    while True:
+while True:
+
+    """
         # Task 20: Using the appropriate function in the module tui, display a menu of options
         # for the different operations that can be performed on the data.
         # Assign the selected option to a suitable local variable
-        # TODO: Your code here
+        """
+    # TODO: Your code here
 
+    from tui import menu
+
+    """
         # Task 21: Check if the user selected the option for loading data.  If so, then do the following:
         # - Use the appropriate function in the module tui to display a message to indicate that the data loading
         # operation has started.
@@ -42,6 +51,9 @@ run()
         # - Read each line from the CSV file and add it to the list 'records'. You should appropriately handle the case
         # where the file cannot be found
         # TODO: Your code here
+    """
+
+
 
         # Task 22: Check if the user selected the option for processing data.  If so, then do the following:
         # - Use the appropriate function in the module tui to display a message to indicate that the data processing
@@ -108,76 +120,77 @@ run()
         #       completed.
         # TODO: Your code here
 
-        # Task 23: Check if the user selected the option for visualising data.  If so, then do the following:
-        # - Use the appropriate function in the module tui to indicate that the data visualisation operation
-        # has started.
-        # - Visualise the data (see below).
-        # - Use the appropriate function in the module tui to display a message to indicate that the data visualisation
-        # operation has completed.
-        #
-        # To visualise the data, it is recommended that you create and call one or more separate functions that do the
-        # following:
-        # - Use the appropriate function in the module tui to retrieve the type of visualisation to display.
-        # - Check what option has been selected
-        #
-        #   - if the user selected the option to visualise the entity type then
-        #       - Use the appropriate function in the module tui to indicate that the entity type visualisation
-        #       process has started.
-        #       - Use your code from earlier to assemble a dictionary containing a list of planets and a list of
-        #       non-planets.
-        #       - Use the appropriate function in the module visual to display a pie chart for the number of planets
-        #       and non-planets
-        #       - Use the appropriate function in the module tui to indicate that the entity type visualisation
-        #       process has completed.
-        #
-        #   - if the user selected the option to visualise the entity gravity then
-        #       - Use the appropriate function in the module tui to indicate that the entity gravity visualisation
-        #       process has started.
-        #       - Use your code from earlier to assemble a dictionary containing lists of entities grouped into
-        #       low (below lower limit), medium and high (above upper limit) gravity categories.
-        #       - Use the appropriate function in the module visual to display a bar chart for the gravities
-        #       - Use the appropriate function in the module tui to indicate that the entity gravity visualisation
-        #       process has completed.
-        #
-        #   - if the user selected the option to visualise the orbit summary then
-        #       - Use the appropriate function in the module tui to indicate that the orbit summary visualisation
-        #       process has started.
-        #       - Use your code from earlier to assemble a nested dictionary of orbiting planets.
-        #       - Use the appropriate function in the module visual to display subplots for the orbits
-        #       - Use the appropriate function in the module tui to indicate that the orbit summary visualisation
-        #       process has completed.
-        #
-        #   - if the user selected the option to animate the planet gravities then
-        #       - Use the appropriate function in the module tui to indicate that the gravity animation visualisation
-        #       process has started.
-        #       - Use your code from earlier to assemble a dictionary containing lists of entities grouped into
-        #       low (below lower limit), medium and high (above upper limit) gravity categories.
-        #       - Use the appropriate function in the module visual to animate the gravity.
-        #       - Use the appropriate function in the module tui to indicate that the gravity animation visualisation
-        #       process has completed.
-        # TODO: Your code here
+"""
+# Task 23: Check if the user selected the option for visualising data.  If so, then do the following:
+# - Use the appropriate function in the module tui to indicate that the data visualisation operation
+# has started.
+# - Visualise the data (see below).
+# - Use the appropriate function in the module tui to display a message to indicate that the data visualisation
+# operation has completed.
 
-        # Task 28: Check if the user selected the option for saving data.  If so, then do the following:
-        # - Use the appropriate function in the module tui to indicate that the save data operation has started.
-        # - Save the data (see below)
-        # - Use the appropriate function in the module tui to indicate that the save data operation has completed.
-        #
-        # To save the data, you should demonstrate the application of OOP principles including the concepts of
-        # abstraction and inheritance.  You should create an AbstractWriter class with abstract methods and a concrete
-        # Writer class that inherits from the AbstractWriter class.  You should then use this to write the records to
-        # a plaintext file using a suitable structure e.g. all the planets followed by non-planets in alphabetical
-        # order.
-        #
-        # TODO: Your code here
+# To visualise the data, it is recommended that you create and call one or more separate functions that do the
+# following:
+# - Use the appropriate function in the module tui to retrieve the type of visualisation to display.
+# - Check what option has been selected
 
-        # Task 29: Check if the user selected the option for exiting.  If so, then do the following:
-        # break out of the loop
-        # TODO: Your code here
+#   - if the user selected the option to visualise the entity type then
+#       - Use the appropriate function in the module tui to indicate that the entity type visualisation
+#       process has started.
+#       - Use your code from earlier to assemble a dictionary containing a list of planets and a list of
+#       non-planets.
+#       - Use the appropriate function in the module visual to display a pie chart for the number of planets
+#       and non-planets
+#       - Use the appropriate function in the module tui to indicate that the entity type visualisation
+#       process has completed.
 
-        # Task 30: If the user selected an invalid option then use the appropriate function of the module tui to
-        # display an error message
-        # TODO: Your code here
+#   - if the user selected the option to visualise the entity gravity then
+#       - Use the appropriate function in the module tui to indicate that the entity gravity visualisation
+#       process has started.
+#       - Use your code from earlier to assemble a dictionary containing lists of entities grouped into
+#       low (below lower limit), medium and high (above upper limit) gravity categories.
+#       - Use the appropriate function in the module visual to display a bar chart for the gravities
+#       - Use the appropriate function in the module tui to indicate that the entity gravity visualisation
+#       process has completed.
 
+#   - if the user selected the option to visualise the orbit summary then
+#       - Use the appropriate function in the module tui to indicate that the orbit summary visualisation
+#       process has started.
+#       - Use your code from earlier to assemble a nested dictionary of orbiting planets.
+#       - Use the appropriate function in the module visual to display subplots for the orbits
+#       - Use the appropriate function in the module tui to indicate that the orbit summary visualisation
+#       process has completed.
+
+#   - if the user selected the option to animate the planet gravities then
+#       - Use the appropriate function in the module tui to indicate that the gravity animation visualisation
+#       process has started.
+#       - Use your code from earlier to assemble a dictionary containing lists of entities grouped into
+#       low (below lower limit), medium and high (above upper limit) gravity categories.
+#       - Use the appropriate function in the module visual to animate the gravity.
+#       - Use the appropriate function in the module tui to indicate that the gravity animation visualisation
+#       process has completed.
+# TODO: Your code here
+
+# Task 28: Check if the user selected the option for saving data.  If so, then do the following:
+# - Use the appropriate function in the module tui to indicate that the save data operation has started.
+# - Save the data (see below)
+# - Use the appropriate function in the module tui to indicate that the save data operation has completed.
+
+# To save the data, you should demonstrate the application of OOP principles including the concepts of
+# abstraction and inheritance.  You should create an AbstractWriter class with abstract methods and a concrete
+# Writer class that inherits from the AbstractWriter class.  You should then use this to write the records to
+# a plaintext file using a suitable structure e.g. all the planets followed by non-planets in alphabetical
+# order.
+#
+# TODO: Your code here
+
+# Task 29: Check if the user selected the option for exiting.  If so, then do the following:
+# break out of the loop
+# TODO: Your code here
+
+# Task 30: If the user selected an invalid option then use the appropriate function of the module tui to
+# display an error message
+# TODO: Your code here
+"""
 
 if __name__ == "__main__":
     run()
